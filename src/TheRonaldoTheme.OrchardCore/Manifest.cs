9da6287b5069dc564e7616cebf6f8ef89635cdcd
Kshitiz Shakya <kshitiz.shakya22@gmail.com>
2020-08-16 19:26:37 +0545

    Fixed paths and manifest file

--- src/TheRonaldoTheme.OrchardCore/Manifest.cs
+++ src/TheRonaldoTheme.OrchardCore/Manifest.cs
@@ -1,9 +1,10 @@
 using OrchardCore.DisplayManagement.Manifest;
 
 [assembly: Theme(
-    Name = "TheRonaldoTheme",
+    Name = "The Ronaldo Theme",
     Author = "Kshitiz Shakya",
     Website = "https://kshitizshakya.com.np",
-    Version = "0.0.1",
-    Description = "Adaptation of the Ronaldo Theme"
+    Version = "1.0.0-rc2",
+    Description = "Adaptation of the Ronaldo Theme",
+    Tags = new[] { "Landing Page", "Blog" }
 )]
